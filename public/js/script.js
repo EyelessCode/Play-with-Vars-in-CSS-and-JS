708f82f81fa0881c62d0b1df2d1bdb5624944813
EyelessCode <cristhianjosue03@outlook.com>
2026-03-26 19:49:15 -0500

    Se agregó el cambio dinámico para las funciones. Hubo ligeros cambios en estructura y estilizado
    Se utilizó el hexadecimal para poder marcar mejor el color, se agregó el script y se agregó un par de atributos de las etiquestas de HTML.

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+const inputs=document.querySelectorAll(".controls input")
+
+const handleUpdate=(e)=>{
+  const suffix=e.explicitOriginalTarget.dataset.sizing||''
+  document.documentElement.style.setProperty(`--${e.target.name}`,
+    e.target.value+suffix)
+}
+
+inputs.forEach((input)=>input.addEventListener("change",handleUpdate))
+inputs.forEach((input)=>input.addEventListener("mousemove",handleUpdate))
